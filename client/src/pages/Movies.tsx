aab731491653225249c104e26f75709adf775c05
Monique <skaiwilliams85@gmail.com>
2023-05-29 10:48:04 -0500

    displayed movies on movie page

--- client/src/pages/Movies.tsx
+++ client/src/pages/Movies.tsx
@@ -1,3 +1,4 @@
+import Film from "../components/film/Film";
 import SlideShow from "../helpers/swiper/SlideShow";
 import { movies } from "../helpers/swiper/movieImages";
 
@@ -5,6 +6,12 @@ const Movies = () => {
   return (
     <div>
       <SlideShow movies={movies} />
+      <Film
+        heading="Popular Movies"
+        url="https://api.themoviedb.org/3/movie/popular"
+        queryKey="popularMovies"
+        mediaType="movie"
+      />
     </div>
   );
 };
